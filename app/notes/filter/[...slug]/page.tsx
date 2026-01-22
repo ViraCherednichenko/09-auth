85e450a733582ba3b0804712bc177a7a6ab6c2bc
vira.cherednichenko <vera.cherednichenko88@gmail.com>
2026-01-22 22:35:12 +0200

    add Notes.client file for filter route

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import NotesClient from "../../NotesClient";
+import NotesClient from "./Notes.client";
 
 const APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000";
 
